35a5ae4d8f3c6e975bd164ff99763ba809a13960
Justin Liu <justinliu1308@gmail.com>
2023-03-07 17:42:35 -0500

    format query, question lines, remove symbols

--- main.py.py
+++ main.py.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import textwrap
 from bs4 import BeautifulSoup
 
 ##################################################################################################
@@ -16,7 +17,35 @@ def main(link):
         problem_name = split_link[4]
 
         # Query for leetcode.com/graphql
-        data = {"operationName":"questionData","variables":{"titleSlug":problem_name},"query":"query questionData($titleSlug: String!) {\n  question(titleSlug: $titleSlug) {\n    questionId\n    questionFrontendId\n    boundTopicId\n    title\n    titleSlug\n    content\n    translatedTitle\n    translatedContent\n    isPaidOnly\n    difficulty\n    likes\n    dislikes\n    isLiked\n    similarQuestions\n    contributors {\n      username\n      profileUrl\n      avatarUrl\n      __typename\n    }\n    langToValidPlayground\n    topicTags {\n      name\n      slug\n      translatedName\n      __typename\n    }\n    companyTagStats\n    codeSnippets {\n      lang\n      langSlug\n      code\n      __typename\n    }\n    stats\n    hints\n    solution {\n      id\n      canSeeDetail\n      __typename\n    }\n    status\n    sampleTestCase\n    metaData\n    judgerAvailable\n    judgeType\n    mysqlSchemas\n    enableRunCode\n    enableTestMode\n    envInfo\n    libraryUrl\n    __typename\n  }\n}\n"}
+        query_question_data = (
+            "query questionData($titleSlug: String!) {"
+                "\n  question(titleSlug: $titleSlug) {"
+                    "\n    questionId\n    questionFrontendId\n    boundTopicId\n    title\n    titleSlug\n    content\n    "
+                    "translatedTitle\n    translatedContent\n    isPaidOnly\n    difficulty\n    likes\n    dislikes\n    "
+                    "isLiked\n    similarQuestions\n    contributors {"
+                        "\n      username\n      profileUrl\n      avatarUrl\n      __typename\n    "
+                    "}\n    "
+                    "langToValidPlayground\n    topicTags {"
+                        "\n      name\n      slug\n      translatedName\n      __typename\n    "
+                    "}\n    "
+                    "companyTagStats\n    codeSnippets {"
+                        "\n      lang\n      langSlug\n      code\n      __typename\n    "
+                    "}\n    "
+                    "stats\n    hints\n    solution {"
+                        "\n      id\n      canSeeDetail\n      __typename\n    "
+                    "}\n    "
+                    "status\n    sampleTestCase\n    metaData\n    judgerAvailable\n    judgeType\n    mysqlSchemas\n    "
+                    "enableRunCode\n    enableTestMode\n    envInfo\n    libraryUrl\n    __typename\n  "
+                "}\n"
+            "}\n"    
+        )
+        data = {
+            "operationName": "questionData",
+            "variables": {
+                "titleSlug": problem_name
+            },
+            "query": query_question_data
+        }
         r = requests.post('https://leetcode.com/graphql', json = data).json()
         soup = BeautifulSoup(r['data']['question']['content'], 'lxml')
 
@@ -26,9 +55,9 @@ def main(link):
         filename = number + ' ' + title
         difficulty = r['data']['question']['difficulty']
         question =  soup.get_text().replace('\n',' ')
-        question = question.split('Example')[0]
-        
-        return filename, difficulty, question
+        question = question.split('Example')[0][:-1]        # splice to drop the unnecessary special character
+        question_lines = textwrap.wrap(question, width = 135)
+        return filename, difficulty, question_lines
 
     def check_file_exists(filename):
         # Check if filename already exists, and if yes, do nothing to prevent file overwrite and data loss
@@ -37,19 +66,22 @@ def main(link):
         else:
             return False
 
-    def create_local_file(filename, link, difficulty, question):
+    def create_local_file(filename, link, difficulty, question_lines):
         # Create a LeetCode problem local Python file
         if 'description' in link: link = link[:-12]     # https:// ...problem-name/description/ isn't necessary, just ..problem-name/ is sufficient
         new_file = open(filename, 'w')
-        new_file.write('# ' + link + '\n# ' + difficulty + '\n\n# ' + question + '\n\n# Your code:\n\n')
+        new_file.write('# ' + link + '\n# ' + difficulty + '\n\n')
+        for line in question_lines:
+            new_file.write('# ' + line + '\n')
+        new_file.write('\n# Your code:\n\n')
         new_file.close()
 
-    filename, difficulty, question = api_query(link)
+    filename, difficulty, question_lines = api_query(link)
     exist = check_file_exists(filename)
     if exist == True:
         print("File already exists. Nothing was done in order to prevent file overwrite.\nOpening existing file...")
     else:
-        create_local_file(filename, link, difficulty, question)
+        create_local_file(filename, link, difficulty, question_lines)
         print("New file created:", filename, "\nOpening new file...")
 
     # Open the file in new tab
