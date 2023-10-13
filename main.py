a207375d07d8042b933ebe2911080fde8be126a2
Justin Liu <justinliu1308@gmail.com>
2023-10-12 23:07:01 -0400

    hotfix + use urllib

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
-import requests
 import os
 import sys
 import textwrap
+from urllib.parse import urlparse, urlunparse
+
+import requests
 from bs4 import BeautifulSoup
 
-def main(link):
-    def api_query(link):
+
+def main(url):
+    def api_query(url):
         # Format problem name for query and obtain structured filename for local file creation
 
-        # Extract problem name (ex: find-the-town-judge) from URL for query
-        split_link = link.split('/')
-        problem_name = split_link[4]
+        # Parse the URL to extract the problem name (ex: find-the-town-judge) for query
+        parsed_url = urlparse(url)
+        
+        # Split the path into segments
+        path_segments = parsed_url.path.strip('/').split('/')
+        problem_name = path_segments[1]
 
+        # Keep the first two path segments and discard the rest
+        truncated_path = '/'.join(path_segments[:2])
+
+        # Create a new URL with the truncated path
+        truncated_url = urlunparse((parsed_url.scheme, parsed_url.netloc, truncated_path, '', '', ''))
+        
         # Query for leetcode.com/graphql
         query_question_data = (
             "query questionData($titleSlug: String!) {"
@@ -53,7 +65,7 @@ def main(link):
         question =  soup.get_text().replace('\n',' ')
         question = question.split('Example')[0][:-1]        # splice to drop the unnecessary special character
         question_lines = textwrap.wrap(question, width = 135)
-        return filename, difficulty, question_lines
+        return filename, difficulty, question_lines, truncated_url
 
     def check_file_exists(filename):
         # Check if filename already exists, and if yes, do nothing to prevent file overwrite and data loss
@@ -62,46 +74,52 @@ def main(link):
         else:
             return False
 
-    def create_local_file(filename, link, difficulty, question_lines):
+    def create_local_file(filename, difficulty, question_lines, url):
         # Create a LeetCode problem local Python file
-        if 'description' in link: link = link[:-12]     # https:// ...problem-name/description/ isn't necessary, just ..problem-name/ is sufficient
         new_file = open(filename, 'w')
-        new_file.write('# ' + link + '\n# ' + difficulty + '\n\n')
+        new_file.write('# ' + url + '\n# ' + difficulty + '\n\n')
         for line in question_lines:
             new_file.write('# ' + line + '\n')
         new_file.write('\n# My code:\n\n')
         new_file.close()
 
-    filename, difficulty, question_lines = api_query(link)
+
+    # Running the program
+    filename, difficulty, question_lines, truncated_url = api_query(url)
     exist = check_file_exists(filename)
     if exist == True:
         print(f"{filename} already exists. Nothing was done in order to prevent file overwrite.\nPulling existing file...")
     else:
-        create_local_file(filename, link, difficulty, question_lines)
+        create_local_file(filename, difficulty, question_lines, truncated_url)
         print("New file created:", filename, "\nOpening new file...")
 
     # Open the file in new tab
     os.startfile(filename)
 
 if __name__ == "__main__":
-    # https://leetcode.com/problems/same-tree/
     if len(sys.argv) == 2:
-        link = sys.argv[1]
-        main(link)
+        link = str(sys.argv[1])
+        try:
+            main(link)
+        except:
+            print("Fatal error creating LeetCode file. For basic troubleshooting, remove except block and view the error.")
     else:
-        print("\nPlease provide one command line argument.\n")
+        print("Please provide a valid website URL as an argument to run the program.\n")
+
 
 
     # Testing - will generate 10 different files. Need to visually verify.
 
     # main('https://leetcode.com/problems/same-tree/')
-    # main('https://leetcode.com/problems/minimum-falling-path-sum/')
+    # main('https://leetcode.com/problems/minimum-falling-path-sum/description/')
     # main('https://leetcode.com/problems/minimum-path-sum/')
-    # main('https://leetcode.com/problems/product-of-the-last-k-numbers/description/')
+    # main('https://leetcode.com/problems/product-of-the-last-k-numbers/description')
     # main('https://leetcode.com/problems/number-of-arithmetic-triplets/')
     # main('https://leetcode.com/problems/stone-game-ii/')
     # main('https://leetcode.com/problems/stone-game-ix/')
     # main('https://leetcode.com/problems/can-i-win/')
     # main('https://leetcode.com/problems/satisfiability-of-equality-equations/')
     # main('https://leetcode.com/problems/word-search/')
-    
+    # main('https://leetcode.com/problems/find-the-difference/?envType=daily-question')
+    # main('https://leetcode.com/problems/summary-ranges/?envType=daily-question&envId=2023-09-25')
+    # main('https://leetcode.com/problems/design-parking-system/?envType=daily-question&envId=2023-09-25')
